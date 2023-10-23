cfa97097bcb7a4fd4dee524021e2fa8b05828441
Hajarita12 <120518556+Hajarita12@users.noreply.github.com>
2023-10-23 23:04:19 +0100

    commit
    date

---
+++ src/test/java/com/example/TpSpring/TpSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TpSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
